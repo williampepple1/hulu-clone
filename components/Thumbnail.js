63c744ec80328cc5a8de195a8682e9035c874270
William Pepple <36817380+williampepple1@users.noreply.github.com>
2021-08-02 11:51:53 +0100

    first commit

---
+++ components/Thumbnail.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import {ThumbUpIcon} from "@heroicons/react/outline"
+import {forwardRef} from "react"
+
+const Thumbnail = forwardRef(({result}, ref) => {
+    const BASE_URL = "https://image.tmdb.org/t/p/original";
+    
+    return (
+        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 sm:transition hover:z-50">
+            <Image 
+            layout="responsive"
+            height={1080}
+            width={1920}
+            src={
+                `${BASE_URL}${result.backdrop_path || result.poster_path}` || `${BASE_URL}${result.poster_path}`
+            }
+            />
+            <div className="p-2">
+                <p className="truncate max-w-md">{result.overview}</p>
+                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+                    {result.title || result.original_name}</h2>
+            </div>
+        </div>
+    )
+})
+
+export default Thumbnail
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,16 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['group-hover'],
   },
-  plugins: [],
+},
+  plugins: [require('tailwind-scrollbar-hide')],
 }
